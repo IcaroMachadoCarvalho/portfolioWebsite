c85ee23c49a78a36957626232a3d8577d0c54ed2
IcaroMachadoCarvalho <icarocarvalho028@gmail.com>
2025-04-02 14:52:25 -0300

    feat:Adicionar formulário de contato reativo

--- src/app/views/forms/forms.component.ts
+++ src/app/views/forms/forms.component.ts
@@ -1,28 +1,34 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { ReactiveFormsModule,FormBuilder, FormGroup, Validators } from '@angular/forms';  // Correção na importação
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-forms',
+  imports:[ReactiveFormsModule, RouterLink, NgClass, NgIf], // importa módulos necessários para o funcionamento
   templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.scss'], 
-  standalone: true,  // Definindo o componente como standalone
+  styleUrls: ['./forms.component.scss'],
+  standalone: true,
 })
 export class FormsComponent {
-  forms: FormGroup;
+  form!: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private formBuilder: FormBuilder) {
     // Usando o FormBuilder para criar o FormGroup com validadores
-    this.forms = this.fb.group({
-      nome: ['', [Validators.required]],  // Validador de 'required'
-      email: ['', [Validators.email]],  // Validador de 'email'
-      mensagem: ['', [Validators.minLength(10)]]  // Validador de 'minLength'
+    this.form = this.formBuilder.group({
+      nome: ['', [Validators.minLength(3),Validators.required]],
+      email: ['', [Validators.email, Validators.required]],
+      mensagem: ['', [Validators.minLength(10), Validators.required]]
     });
+    window.scrollTo(0,0);
   }
 
   // Função para enviar a mensagem
   sendMessage() {
-    if (this.forms.valid) {
-      console.log(this.forms.value);
+    if (this.form.valid) {
+      console.log(this.form.value);
+      this.form.reset();
+      alert("Enviado com sucesso")
     } else {
       console.log('Formulário inválido');
     }
